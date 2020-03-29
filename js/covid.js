7cc90ee9e285f414752998de74bda7eb1483d78d
Danilo Lekovic <danilo@lekovic.ca>
2020-03-28 17:17:50 -0700

    New, much more reliable source. New information

--- js/covid.js
+++ js/covid.js
@@ -1,16 +1,8 @@
-$.getJSON("https://pomber.github.io/covid19/timeseries.json", function(data) {
-    jsonToday = data.Canada[data.Canada.length - 1];
-    $("#confirmed").html(jsonToday.confirmed);
-    $("#deaths").html(jsonToday.deaths);
-    $("#recoveries").html(jsonToday.recovered);
-
-    jsonYesterday = data.Canada[data.Canada.length - 2];
-    $("#newCases").html(jsonToday.confirmed - jsonYesterday.confirmed);
-    $("#newDeaths").html(jsonToday.deaths - jsonYesterday.deaths);
-    $("#newRecoveries").html(jsonToday.recovered - jsonYesterday.recovered);
-
-    json2weeks = data.Canada[data.Canada.length - 14];
-    $("#newCases2").html(jsonToday.confirmed - json2weeks.confirmed);
-    $("#newDeaths2").html(jsonToday.deaths - json2weeks.deaths);
-    $("#newRecoveries2").html(jsonToday.recovered - json2weeks.recovered);
+$.getJSON("https://corona.lmao.ninja/countries/Canada", function(data) {
+    $("#confirmed").html(data.cases);
+    $("#deaths").html(data.deaths);
+    $("#recoveries").html(data.recovered);
+    $("#newCases").html(data.todayCases);
+    $("#newDeaths").html(data.todayDeaths);
+    $("#perMillionCases").html(data.casesPerOneMillion);
 });
