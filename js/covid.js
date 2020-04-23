968131cea6845d7097eb46aab18496d36b1b5fde
Danilo Lekovic <danilo@lekovic.ca>
2020-04-22 21:49:33 -0700

    commas. s/o nathan

--- js/covid.js
+++ js/covid.js
@@ -2,11 +2,11 @@ let totalCases = undefined;
 
 $.getJSON("https://corona.lmao.ninja/v2/countries/Canada", function(data) {
     totalCases = data.cases;
-    $("#confirmed").html(data.cases);
-    $("#deaths").html(data.deaths);
-    $("#recoveries").html(data.recovered);
-    $("#newCases").html(data.todayCases);
-    $("#newDeaths").html(data.todayDeaths);
+    $("#confirmed").html(data.cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $("#deaths").html(data.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $("#recoveries").html(data.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $("#newCases").html(data.todayCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $("#newDeaths").html(data.todayDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 });
 
 $("g").on("click", function() {
@@ -32,17 +32,13 @@ $("g").on("click", function() {
         $("#province3").html("Approximately <span><img src='./img/loading.gif' alt='...'></span>% of Canada's total cases are in <span>" + province + "</span>.");    
 
         $.getJSON("https://corona.lmao.ninja/v2/historical/CA/" + province.split(" ").join("%20") + "?lastdays=2", function(data) {
-            console.log(yesterday);
-            console.log(today);
-            console.log(data["timeline"]["cases"]);
-            
             if (data["timeline"]["cases"][today] == undefined) {
                 $("#province").html("At the moment, the numbers of COVID-19 cases or deaths in <span>" + province + "</span> are unavailable.");
-                $("#province2").html("In total, there have been <span>" + data["timeline"]["cases"][yesterday] + "</span> cases and <span>" + data["timeline"]["deaths"][yesterday] + "</span> deaths in <span>" + province + "</span>.");
+                $("#province2").html("In total, there have been <span>" + data["timeline"]["cases"][yesterday].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</span> cases and <span>" + data["timeline"]["deaths"][yesterday].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</span> deaths in <span>" + province + "</span>.");
                 $("#province3").html("Approximately " + parseFloat(data["timeline"]["cases"][yesterday] / totalCases * 100).toFixed(2) + "% of Canada's total cases are in <span>" + province + "</span>.");    
             } else {
-                $("#province").html("Today, there have been <span>" + (data["timeline"]["cases"][today] - data["timeline"]["cases"][yesterday]) + "</span> reported cases and <span>" + (data["timeline"]["deaths"][today] - data["timeline"]["deaths"][yesterday]) + "</span> deaths in <span>" + province + "</span>.");
-                $("#province2").html("In total, there have been <span>" + data["timeline"]["cases"][today] + "</span> cases and <span>" + data["timeline"]["cases"][today] + "</span> deaths in <span>" + province + "</span>.");
+                $("#province").html("Today, there have been <span>" + (data["timeline"]["cases"][today] - data["timeline"]["cases"][yesterday]).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</span> reported cases and <span>" + (data["timeline"]["deaths"][today] - data["timeline"]["deaths"][yesterday]).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</span> deaths in <span>" + province + "</span>.");
+                $("#province2").html("In total, there have been <span>" + data["timeline"]["cases"][today].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</span> cases and <span>" + data["timeline"]["cases"][today].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</span> deaths in <span>" + province + "</span>.");
                 $("#province3").html("Approximately " + parseFloat(data["timeline"]["cases"][today] / totalCases * 100).toFixed(2) + "% of Canada's total cases are in <span>" + province + "</span>.");    
             }
         });
